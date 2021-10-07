ac5786409525548bb2449d3f46a1ed476cd8e0ee
Grosse Riese <plmman2010@gmail.com>
2021-10-07 11:01:24 +0200

    update
    update

--- my_python_files/LAB_1/lab_numerisk_losning_ekvationer.py
+++ my_python_files/LAB_1/lab_numerisk_losning_ekvationer.py
@@ -142,3 +142,150 @@ _x_ = min_bisect(y,-1,0)
 print("Solution Found: x = {}, f(x) = {} ".format(round(_x_,5),round(y(_x_), 5) ))
 
 ####################################################################################################
+####################################################################################################
+#uppgift_5
+
+def projektilbanan(x):
+    y0=1.85;
+    v0=10; 
+    theta=45; 
+    g=9.81 #Antaganden
+    
+    t=theta*np.pi/180 #Konvertering från grader till radianer
+    
+    a=g/(2*v0**2*np.cos(t)**2);
+    b=v0**2*np.sin(2*t)/(2*g);
+    c=v0**2*np.sin(t)**2/(2*g)
+    
+    return y0-a*(x-b)**2+c #Returnerar projektilens höjd vid (horisontellt) avstånd x från utkastpunkten
+
+def draw_graf():
+    x = np.linspace(-4,5) # a list of x-value
+    plt.plot(x,projektilbanan(x)) # plot values of (x,y)
+    plt.text(6.1,0.5, "$y(x)=f(x)$",fontsize=16) 
+    plt.axis([-3,6,-1,6]) 
+    plt.grid() #Aktiverar rutnät
+    
+    x2 = min_bisect(projektilbanan,-2,-1)
+    print("Solution Found: x2 = {}, f(x) = {} ".format(round(x2,5),round(projektilbanan(x2), 5) ))
+    
+#draw_graf()
+##############################################################
+#uppgift_6
+
+def derivative(f, x, dx = 1e-6):
+    df = f(x + dx) - f(x - dx)
+    return df/(2*dx)
+
+def newton_raphsons(f, x0, tol = 1e-10, maxit = 100): #tol:tolerance
+
+    x = x0
+    fx = f(x)
+
+    for _ in range(maxit):
+
+        if abs(fx) < tol:
+            #print("fx {} ".format(fx))
+            break
+        
+        fpx = derivative(f, x)
+        if abs(fpx) < tol:
+            #print("fpx {} ".format(fx))
+            break
+
+        x = x - fx/fpx
+        fx = f(x)
+
+    return x
+
+func = lambda x: x**3 - np.cos(4*x)
+
+def draw_newton_graf():
+    x = np.linspace(-2,0.58) # a list of x-value
+    plt.plot(x,func(x)) # plot values of (x,y)
+    plt.text(4.5,0.5, "$Y=f(x)$",fontsize=16) 
+    plt.axis([-1.5,4,-0.5,1]) 
+    plt.grid() #Aktiverar rutnät
+    
+    x0 = 0.5
+    x_k = newton_raphsons(func, x0, tol = 1e-10, maxit = 100)
+    print("Solution: x = {}, f(x) = {}".format(x_k, func(x_k)))
+    
+#draw_newton_graf()
+
+##############################################################
+#uppgift_7
+def derivative_function(f, x, dx = 1e-6):
+    df = f(x + dx) - f(x - dx)
+    return df/(2*dx)
+
+def min_NewtonRaphson(f,Df,x0,tol = 1e-10):
+    x = x0
+    fx = f(x)
+
+    for _ in range(100):
+
+        if abs(fx) < tol:
+            #print("fx {} ".format(fx))
+            break
+        
+        fpx = Df(f, x)
+        if abs(fpx) < tol:
+            #print("fpx {} ".format(fx))
+            break
+
+        x = x - fx/fpx
+        fx = f(x)
+
+    return x
+
+func_7 = lambda x: x
+def draw_min_NewtonRaphson_graf():
+    x = np.linspace(-1,5) # a list of x-value
+    plt.plot(x,func_7(x)) # plot values of (x,y)
+    plt.text(4.5,0.5, "$y=f(x)$",fontsize=16) 
+    plt.axis([-1,4,-1,5]) 
+    plt.grid() #Aktiverar rutnät
+    
+    x0 = 2
+    x_k = min_NewtonRaphson(func_7,derivative_function,x0, tol = 1e-10)
+    print("Solution: x = {}, f(x) = {}".format(x_k, func_7(x_k)))
+    
+#draw_min_NewtonRaphson_graf()
+#############################################################################################
+##=____________________________________________________________________________________##
+""" uppgift_8 : Testa nu din funktion min_NewtonRaphson på följande funktioner 
+genom att rita grafer samt beräkna samtliga nollställen:"""
+#########################################################################################
+func_8_a = lambda x: 0.5*(x-2)**2-2*np.cos(2*x)-1.5
+""" upp.8_A"""
+def draw_min_NewtonRaphson_8_a_graf():
+    x = np.linspace(-1,5) # a list of x-value
+    plt.plot(x,func_8_a(x)) # plot values of (x,y)
+    plt.text(4,0.5, "$y=f(x)$",fontsize=16) 
+    plt.axis([-1,4,-4,3]) 
+    plt.grid() #Aktiverar rutnät
+    
+    x0 = 2
+    x_k = min_NewtonRaphson(func_8_a,derivative_function,x0, tol = 1e-10)
+    print("Solution: x = {}, f(x) = {}".format(x_k, func_8_a(x_k)))
+    
+draw_min_NewtonRaphson_8_a_graf()
+
+
+
+""" upp.8_B"""
+
+func_8_b = lambda x: x**3-np.cos(4*x)
+def draw_min_NewtonRaphson_8_b_graf():
+    x = np.linspace(-1,5) # a list of x-value
+    plt.plot(x,func_8_b(x)) # plot values of (x,y)
+    plt.text(4,0.5, "$Y=f(x)$",fontsize=16) 
+    plt.axis([-1,2.5,-1.5,4.5]) 
+    plt.grid() #Aktiverar rutnät
+    
+    x0 = 2
+    x_k = min_NewtonRaphson(func_8_b,derivative_function,x0, tol = 1e-10)
+    print("Solution: x = {}, f(x) = {}".format(x_k, func_8_b(x_k)))
+    
+draw_min_NewtonRaphson_8_b_graf()
